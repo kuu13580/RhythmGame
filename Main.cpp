824a3e756a9ce9adbcdc8a62072c429b8e3c5614
kuu13580 <13580kuu@gmail.com>
2023-02-04 01:09:11 +0900

    init

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+﻿# include "common.h"
+#include "Title.h"
+#include "Game.h"
+
+void Main()
+{
+	App manager;
+	manager.add<Title>(State::Title);
+	manager.add<Game>(State::Game);
+
+	manager.init(State::Game);
+
+	while (System::Update())
+	{
+		if (not manager.update())
+		{
+			break;
+		}
+	}
+}
---
+++ common.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "stdafx.h"
+
+// シーン名
+enum class State
+{
+	Title,
+	Game,
+};
+
+// 共有するデータ
+struct GameData
+{
+	
+};
+
+// ノーツのデータ
+typedef struct NoteData {
+	int32 time; // 設置時間(ms)
+	int32 line; // ライン
+}NoteData;
+
+// デフォルトのノーツスピード px / s
+constexpr double DEFAULT_SPEED = 100;
+
+
+using App = SceneManager<State, GameData>;
