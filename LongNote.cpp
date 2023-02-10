586382ed962db8bca1b4146a156c20506b162d12
kuu13580 <13580kuu@gmail.com>
2023-02-11 06:07:58 +0900

    LongNoteをNoteBase基底に変更

--- LongNote.cpp
+++ LongNote.cpp
@@ -1,2 +1,16 @@
 ﻿#include "common.h"
 #include "LongNote.h"
+
+CLongNote::CLongNote(Vec2 s_pos, Vec2 e_pos, int32 width, Color color)
+	: CNoteBase(s_pos, width, color), end_pos_(e_pos)
+{
+
+}
+
+void CLongNote::update(const double& speed) {
+
+}
+
+Score checkCollision(const double& speed, const int32& border_line) {
+
+}
--- LongNote.h
+++ LongNote.h
@@ -1,10 +1,16 @@
 ﻿#pragma once
 #include "common.h"
-#include "Note.h"
+#include "NoteBase.h"
 
-class CLongNote : public CNote {
+class CLongNote : public CNoteBase {
 private:
-	CLongNote();
-	~CLongNote();
+	// 描画本体
+	Line longNote_;
+	// 描画終了位置
+	Vec2 end_pos_;
 public:
+	CLongNote(Vec2 s_pos, Vec2 e_pos, int32 width, Color color);
+	void update(const double& speed) override;
+	void draw() override { longNote_.draw(color_); }
+	Score checkCollision(const double& speed, const int32& border_line) override;
 };
